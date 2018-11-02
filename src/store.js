e8b33660cc2383291c0bae50a1e3df7389090493
Marco Montalto Monella <mmm1108@nyu.edu>
2018-11-02 18:12:20 -0400

    Enable Redux Chrome Ext

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 // N.B. when importing index.js, you dont have to specify the file (see below)
@@ -11,7 +11,12 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(...middleware)
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 export default store;
+
+// Video continue from 53:15! 
